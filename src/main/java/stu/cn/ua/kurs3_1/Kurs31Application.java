4da88ba0037cf9bc888911f1c980b3d1d9ce2ab1
ArtomDovgal <artom.dovgalnew@gmail.com>
2022-12-26 17:03:27 +0200

    Created #0

---
+++ src/main/java/stu/cn/ua/kurs3_1/Kurs31Application.java
@@ -0,0 +1,13 @@
+package stu.cn.ua.kurs3_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs31Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs31Application.class, args);
+    }
+
+}
